c266bcca158a85cf71d50c221fb4d2104dce0bbe
“pipe17049” <felipe1368@hotmail.com>
2020-11-14 20:14:04 -0500

    Se hace nueva version, presenta mismo errores, se procesedera a hacerse con colas

--- RoundRobin/RoundRobin.py
+++ RoundRobin/RoundRobin.py
@@ -36,38 +36,46 @@ for i in range(n):
     totaltiempo+=aux1               #Acumulador Tiempo total de ejecucion (Suma todo los tiempo de burst)
     
 
-for timep in range (totaltiempo+1):     #Ciclo for para el tiempo con la variable timep hasta que llege al acumulador + 1
-    if len(ejecucion)==0:               #Pregunta si hay en ejecucion algun elemento
-        ejecucion.append(cont2)             #Agrega a ejecucion el proceso que ingrese
-        cont2+=1                            #Aumenta contador cont2 , que se encarga de la entrada de los procesos
-    elif timep == delay[cont2]:         #Pregunta si el tiempo transcurrido total es igual al delay del proceso que sigue
+for timep in range (totaltiempo+q):     #Ciclo for para el tiempo con la variable timep hasta que llege al acumulador + 1
 
-        cola.append(cont2)                  #Agrega a la cola el proceso que llego
-        cont2+=1                            #Aumenta contador cont2 , que se encarga de la entrada de los procesos
-        if cont2==n: cont2=0                #Si cont2 llega al numero de elementos n, resetear
+    for x in range(len(proceso)):
+
+        if timep == delay[cont2]:                   #Pregunta si el tiempo transcurrido total es igual al delay del proceso que sigue
+            if len(ejecucion)==0:                   #Pregunta si hay en ejecucion algun elemento
+                ejecucion.append(cont2)             #Agrega a ejecucion el proceso que ingrese
+                cont2+=1                            #Aumenta contador cont2 , que se encarga de la entrada de los procesos
+            else:
+                cola.append(cont2)                  #Agrega a la cola el proceso que llego
+                cont2+=1                            #Aumenta contador cont2 , que se encarga de la entrada de los procesos
+            if cont2==n: cont2=0                    #Si cont2 llega al numero de elementos n, resetear
     
-    else: None
+    
 
 
 
     #Caso cuando el tiempo que requiere el proceso en ejecucion es menor que el quantu
-    if cont>=0 and cont < q and cont>=proceso[ejecucion[0]]:             #Compara si la variable cont esta entre 0 y el quantum y si es igual al tiempo de proceso
+    if cont>0 and cont < q and cont>=proceso[ejecucion[0]]:             #Compara si la variable cont esta entre 0 y el quantum y si es igual al tiempo de proceso
         tiempot[ejecucion[0]]=timep                                     #Manda el tiempo que lleva transcurrido desde que llego al arreglo tiempot
         if len(cola)>0:                                                 #Compara si hay elementos en cola
             ejecucion[0]=cola.pop(0)                                        #Elimina el proceso del sistema y pasa el primero de la cola a ejecucion
+        elif len(cola)==0 and len(ejecucion)==1 and proceso[ejecucion[0]]==0:
+            break
         cont=1                                                          #Resetea contador que es el que compara quantum a 1
 
     #Caso cuando el contador de quantum es igual al quantum
     elif cont == q:                                                     #Compara si cont es igual a quantum
+        
         proceso[ejecucion[0]]-=q                                        #Resta en proceso el tiempo que se ejecuto
         if proceso[ejecucion[0]] == 0:                                  #Compara si proceso ejecucion despues de la resta llego a ceo
             tiempot[ejecucion[0]]=timep                                     #Manda el tiempo que lleva transcutrrido desde que llego al arreglo tiempot
             if len(cola)>0:                                                 #Compara si la cola no esta vacia
                 ejecucion[0]=cola.pop(0)                                        #Elimina el proceso del sistema y pasa el primero de la cola a ejecucion
+            else:
+                break
         else:               
-            aux3 = ejecucion[0]                                             #Guardo el No proceso en ejecucion a la variable aux3
+            
             if len(cola)>0:                                                 #Compara si hay elementos en cola
-
+                aux3 = ejecucion[0]                                             #Guardo el No proceso en ejecucion a la variable aux3
                 ejecucion[0]=cola.pop(0)                                        #Para el primer elemento en cola a ejecucion
                 cola.append(aux3)                                               #Pasa el proceso que termino tiempo al final de la cola
             
@@ -82,3 +90,4 @@ diccionario2 =  sorted(diccionario.items(), key=operator.itemgetter(1))
 
 for name in enumerate(diccionario2):
     print(name[1][0],diccionario[name[1][0]])
+
